3cf0109b1fad4b5cd078363d2b848a93bcd343f4
piskuro <Roman.Piskun.extern@brose.com>
2024-07-03 20:13:30 +0000

    Initial commit

---
+++ src/stm32l0xx_it.c
@@ -0,0 +1,46 @@
+#include "stm32l0xx_it.h"
+/******************************************************************************/
+/*           Cortex-M0+ Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable Interrupt.
+  */
+void NMI_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+    // HAL_IncTick();
+}
+
